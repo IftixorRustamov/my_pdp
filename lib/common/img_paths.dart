6e84447f458068f9230ba0ae4bb06ce09cfd2b20
Abdusalom <abdusalom@gmail.com>
2024-12-17 00:19:47 +0500

    Created the assets folder and saved some of the images.

---
+++ lib/common/img_paths.dart
@@ -0,0 +1,22 @@
+class ImgPaths {
+  //Abdusalom images
+  static String appBarLogo = "assets/images/pdp_appbar_logo.png";
+  static String appBarText = "assets/images/pdp_appbar_text.png";
+  static String guyAndGirl = "assets/images/guy_and_girl.png";
+
+
+
+
+
+
+  //Iftihor images
+
+
+
+
+
+
+
+
+  //Abzal images
+}
