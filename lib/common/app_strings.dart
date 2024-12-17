bf72f92f2ccdd71a19306c71f5e80dbd3d14775b
Abdusalom <abdusalom@gmail.com>
2024-12-17 23:43:57 +0500

    Finished Discover Screen.

--- lib/common/app_strings.dart
+++ lib/common/app_strings.dart
@@ -5,4 +5,5 @@ class AppStrings {
   static const String aboutUniversity = "About University";
   static const String eventsAndHolidays = "Events/Holidays";
   static const String info = "info";
+  static const String discover = "Discover";
 }
